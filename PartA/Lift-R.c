91a7140ec3492b22e8dc20e48d8af98d7094a62a
Moritz Bergemann <moritz.bergemann@gmail.com>
2020-04-15 22:22:31 +0800

    Started work on main method. Also started Lift-R though it's probably wrong

--- PartA/Lift-R.c
+++ PartA/Lift-R.c
@@ -1,2 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
 /**Lift-R by Moritz Bergemann
- * File containing functionality 
+ * File containing functionality for reading lift requests from the imported file writing these to the request buffer.
+ */
+
+void request(FILE* file, int bufferSize)
+{
+    FILE* file;
+
+    file = fopen(filename, "r");
+
+    if (file != NULL) //If file successfully opened
+    {
+        
+    }
+    else //If file failed to open
+    {
+        printf("Error - failed to open file")
+    }
+    
+
+    
+}
+
+/** Checks imported filename points to a valid file with the correct number of requests
+ *  (though it does not check whether these requests are valid)
+ */
+FILE* fileCheck(char** path)
+{
+
+}
--- PartA/Main.c
+++ PartA/Main.c
@@ -2,7 +2,72 @@
 #include <pthread.h>
 #include "Main.h"
 
-int main(void)
+const int MIN_REQUESTS = 50;
+const int MAX_REQUESTS = 100;
+const int MAX_SERVICE_LENGTH = 100;
+const char* REQUEST_FILE_NAME = "sim_input";
+
+/** Takes in & validates command line parameters
+ */
+int main(int argc, char** argv)
 {
-    printf("Hello world\n");
+    if (argc == 3)
+    {
+        //Getting & validating command line parameters
+        int bufferSize, serviceLength;
+        int sSuccesses = 0;
+        sSuccesses += sscanf(argv[1], "%d", &bufferSize);
+        sSuccesses += sscanf(argv[2], "%d", &serviceLength);
+        if (sSuccesses == 2)
+        {
+            if (bufferSize >= 1 && bufferSize <= MAX_REQUESTS) //Validating buffer size range
+            {
+                if (serviceLength >= 0 && serviceLength <= MAX_SERVICE_LENGTH) //Validating service length range
+                {
+                    //All settings valid, begin running program
+                    initThreads(bufferSize, serviceLength);
+                }
+                else
+                {
+                    printf("Error: Service duration must be between 1 & %d (inclusive)! Exiting...\n", MAX_SERVICE_LENGTH);
+                }
+                
+            }
+            else
+            {
+                printf("Error: Buffer size must be between 1 & %d (inclusive)! Exiting...\n", MAX_REQUESTS);
+            }
+            
+        }
+        else
+        {
+            printf("Incorrect command line argument format (both must be whole numbers)!\n");
+        }
+        
+    }
+    else
+    {
+        printf("Incorrect number of command line arguments (2 required)!\n");
+        printHelp();
+    }
+}
+
+/** Outputs help for program
+ */
+void printHelp()
+{
+    printf("Lift simulation A by Moritz Bergemann:\n");
+    printf("Run in format \"lift_sim_A m t\"");
+    printf("\t m: Buffer size (must be between 1 & %d)\n", MAX_REQUESTS);
+    printf("\t t: Time required for lift service (must be between 1 & %d)\n", MAX_SERVICE_LENGTH);
+}
+
+/** Initiates threads TODO more info here
+ */
+void initThreads(int bufferSize, int serviceLength)
+{
+    //Validating request file exists & contains correct number of requests
+    FILE* reqFile = checkFile(REQUEST_FILE_NAME);
+
+    
 }
